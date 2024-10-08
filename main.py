bc6fa61163ae1c62961f70d72efd72e2fcfe1f4f
SohaibAShah <asyedsohaib@gmail.com>
2024-10-08 20:52:03 +0500

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from WineQuality import logger
+from WineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 
-logger.info("This is our custome log!")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataIngestionTrainingPipline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
