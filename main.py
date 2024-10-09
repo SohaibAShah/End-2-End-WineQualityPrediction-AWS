5d81e21c2d330739c88478361f6aff8141957499
SohaibAShah <asyedsohaib@gmail.com>
2024-10-09 15:30:55 +0500

    Model Trainer added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from WineQuality import logger
 from WineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 from WineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipline
 from WineQuality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipline
-
+from WineQuality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipline
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
@@ -32,6 +32,18 @@ STAGE_NAME = "Data Transformation Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = DataTransformationTrainingPipline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = ModelTrainerTrainingPipline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
--- src/WineQuality/config/configuration.py
+++ src/WineQuality/config/configuration.py
@@ -2,7 +2,8 @@ from WineQuality.constants import *
 from WineQuality.utils.common import read_yaml, create_directories
 from WineQuality.entity.config_entity import (DataIngestionConfig, 
                                               DataValidationConfig, 
-                                              DataTransformationConfig)
+                                              DataTransformationConfig,
+                                              ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -15,13 +16,13 @@ class ConfigurationManager:
         self.params = read_yaml(params_filepath)
         self.schema = read_yaml(schema_filepath)
 
-        create_directories([self.config.artifacts_root, self.config.data_ingestion.root_dir, self.config.data_validation.root_dir])
+        create_directories([self.config.artifacts_root])
 
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
-        #create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
@@ -37,7 +38,7 @@ class ConfigurationManager:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
 
-        #create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
         data_validation_config = DataValidationConfig(
             root_dir=config.root_dir,
@@ -60,3 +61,24 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+
+        return model_trainer_config
